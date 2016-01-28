a2b43ecbc0962a1f0a6e214a85d2fea4723ecb23
NICHOLAS BAKER <nicholas@rochay.com>
2016-01-28 13:24:36 +0000

    first commit

---
+++ templates/rochaytv2-catalog-backupScript.xml.js
@@ -0,0 +1,169 @@
+/*
+Copyright (C) 2015 Apple Inc. All Rights Reserved.
+See LICENSE.txt for this sample’s licensing information
+
+Abstract:
+A catalog template allows you to display groupings of related items, such as genres of movies or TV shows. View the list of groupings on the left and focus on one to see its items on the right.
+*/
+var Template = function() { return `<?xml version="1.0" encoding="UTF-8" ?>
+  <document>
+    <head>
+      <style>
+      .whiteText {
+        color: rgb(255, 255, 255);
+      }
+      </style>
+    </head>
+    <catalogTemplate>
+        <background>
+            <img src="${this.BASEURL}resources/crest-bkg3.png" />
+        </background>
+      <banner>
+        <background>
+        <img src="${this.BASEURL}resources/rochaytv2-bkg-banner1.png" width="1920" height="360" />
+      </background>
+      </banner>
+      <list>
+        <section>
+          <header>
+            <title>Select Programme</title>
+          </header>
+          <listItemLockup>
+            <title>In Conversation with...</title>
+            <decorationLabel>6</decorationLabel>
+            <relatedContent>
+              <grid>
+                <section>
+                  <lockup vidUrl="https://s3-eu-west-1.amazonaws.com/rochaytv/NaveenTestimonial-A-HD.mov">
+                    <img src="${this.BASEURL}resources/paulmckenna2.lcr" width="548" height="308" />
+                    <title class="whiteText">Paul McKenna</title>
+                  </lockup>
+                  <lockup vidUrl="https://s3-eu-west-1.amazonaws.com/rochaytv/KevinTestimonial-C-HD.mov">
+                    <img src="${this.BASEURL}resources/noel-edmonds.lcr" width="548" height="308" />
+                    <title class="whiteText">Noel Edmonds</title>
+                  </lockup>
+                  <lockup vidUrl="https://s3-eu-west-1.amazonaws.com/rochaytv/NaveenTestimonial-B-HD.mov">
+                    <img src="${this.BASEURL}resources/mark-aldridge.lcr" width="548" height="308" />
+                    <title class="whiteText">Mark Aldridge</title>
+                  </lockup>
+                  <lockup vidUrl="https://s3-eu-west-1.amazonaws.com/rochaytv/KevinTestimonials-D-HD.mov">
+                    <img src="${this.BASEURL}resources/stewart-mcintyre.lcr" width="548" height="308" />
+                    <title class="whiteText">Stewart McIntyre</title>
+                  </lockup>
+                  <lockup>
+                    <img src="${this.BASEURL}resources/richard-lambert.lcr" width="548" height="308" />
+                    <title class="whiteText">Richard Lambert</title>
+                  </lockup>
+                  <lockup>
+                    <img src="${this.BASEURL}resources/nigel-stuart.lcr" width="548" height="308" />
+                    <title class="whiteText">Nigel Stewart</title>
+                  </lockup>
+                </section>
+              </grid>
+            </relatedContent>
+          </listItemLockup>
+          <listItemLockup>
+            <title>Luxury Insights...</title>
+            <decorationLabel>8</decorationLabel>
+            <relatedContent>
+              <grid>
+                <section>
+                  <lockup>
+                    <img src="${this.BASEURL}resources/llx-thumbnail.lcr" width="548" height="308" />
+                    <title class="whiteText">Title 1</title>
+                  </lockup>
+                  <lockup>
+                    <img src="${this.BASEURL}resources/images/music/music_2.lcr" width="548" height="308" />
+                    <title class="whiteText">Title 2</title>
+                  </lockup>
+                  <lockup>
+                    <img src="${this.BASEURL}resources/images/music/music_3.lcr" width="548" height="308" />
+                    <title class="whiteText">Title 3</title>
+                  </lockup>
+                  <lockup>
+                    <img src="${this.BASEURL}resources/images/music/music_4.lcr" width="548" height="308" />
+                    <title class="whiteText">Title 4</title>
+                  </lockup>
+                  <lockup>
+                    <img src="${this.BASEURL}resources/images/music/music_5.lcr" width="548" height="308" />
+                    <title class="whiteText">Title 5</title>
+                  </lockup>
+                  <lockup>
+                    <img src="${this.BASEURL}resources/images/music/music_6.lcr" width="548" height="308" />
+                    <title class="whiteText">Title 6</title>
+                  </lockup>
+                  <lockup>
+                    <img src="${this.BASEURL}resources/images/music/music_7.lcr" width="548" height="308" />
+                    <title class="whiteText">Title 7</title>
+                  </lockup>
+                  <lockup>
+                    <img src="${this.BASEURL}resources/images/music/music_1.lcr" width="548" height="308" />
+                    <title class="whiteText">Title 8</title>
+                  </lockup>
+                </section>
+              </grid>
+            </relatedContent>
+          </listItemLockup>
+          <listItemLockup>
+            <title>Luxury Events...</title>
+            <decorationLabel>12</decorationLabel>
+            <relatedContent>
+              <grid>
+                <section>
+                  <lockup>
+                    <img src="${this.BASEURL}resources/llx-thumbnail.lcr" width="548" height="308" />
+                    <title class="whiteText">Title 1</title>
+                  </lockup>
+                  <lockup>
+                    <img src="${this.BASEURL}resources/images/music/music_2.lcr" width="548" height="308" />
+                    <title class="whiteText">Title 2</title>
+                  </lockup>
+                  <lockup>
+                    <img src="${this.BASEURL}resources/images/music/music_3.lcr" width="548" height="308" />
+                    <title class="whiteText">Title 3</title>
+                  </lockup>
+                  <lockup>
+                    <img src="${this.BASEURL}resources/images/music/music_4.lcr" width="548" height="308" />
+                    <title class="whiteText">Title 4</title>
+                  </lockup>
+                  <lockup>
+                    <img src="${this.BASEURL}resources/images/music/music_5.lcr" width="548" height="308" />
+                    <title class="whiteText">Title 5</title>
+                  </lockup>
+                  <lockup>
+                    <img src="${this.BASEURL}resources/images/music/music_6.lcr" width="548" height="308" />
+                    <title class="whiteText">Title 6</title>
+                  </lockup>
+                  <lockup>
+                    <img src="${this.BASEURL}resources/images/music/music_7.lcr" width="548" height="308" />
+                    <title class="whiteText">Title 7</title>
+                  </lockup>
+                  <lockup>
+                    <img src="${this.BASEURL}resources/images/music/music_1.lcr" width="548" height="308" />
+                    <title class="whiteText">Title 8</title>
+                  </lockup>
+                  <lockup>
+                    <img src="${this.BASEURL}resources/images/music/music_2.lcr" width="548" height="308" />
+                    <title class="whiteText">Title 9</title>
+                  </lockup>
+                  <lockup>
+                    <img src="${this.BASEURL}resources/images/music/music_3.lcr" width="548" height="308" />
+                    <title class="whiteText">Title 10</title>
+                  </lockup>
+                  <lockup>
+                    <img src="${this.BASEURL}resources/images/music/music_4.lcr" width="548" height="308" />
+                    <title class="whiteText">Title 11</title>
+                  </lockup>
+                  <lockup>
+                    <img src="${this.BASEURL}resources/images/music/music_5.lcr" width="548" height="308" />
+                    <title class="whiteText">Title 12</title>
+                  </lockup>
+                </section>
+              </grid>
+            </relatedContent>
+          </listItemLockup>
+        </section>
+      </list>
+    </catalogTemplate>
+  </document>`
+}
---
+++ templates/rochaytv2-catalog.xml.js
@@ -0,0 +1,97 @@
+/*
+Copyright (C) 2015 Apple Inc. All Rights Reserved.
+See LICENSE.txt for this sample’s licensing information
+
+Abstract:
+A catalog template allows you to display groupings of related items, such as genres of movies or TV shows. View the list of groupings on the left and focus on one to see its items on the right.
+*/
+var Template = function() { return `<?xml version="1.0" encoding="UTF-8" ?>
+  <document>
+    <head>
+      <style>
+      .whiteText {
+        color: rgb(255, 255, 255);
+      }
+      </style>
+    </head>
+    <catalogTemplate>
+        <background>
+            <img src="${this.BASEURL}resources/crest-bkg3.png" />
+        </background>
+      <banner>
+        <background>
+        <img src="${this.BASEURL}resources/rochaytv2-bkg-banner1.png" width="1920" height="360" />
+      </background>
+      </banner>
+      <list>
+        <section>
+          <header>
+            <title>Select Programme</title>
+          </header>
+          <listItemLockup>
+            <title>In Conversation with...</title>
+            <decorationLabel>6</decorationLabel>
+            <relatedContent>
+              <grid>
+                <section>
+                  <lockup vidUrl="https://s3-eu-west-1.amazonaws.com/rochaytv/Paul-McKenna-HD1080p.mov">
+                    <img src="${this.BASEURL}resources/paulmckenna2.lcr" width="548" height="308" />
+                    <title class="whiteText">Paul McKenna</title>
+                  </lockup>
+                  <lockup vidUrl="https://s3-eu-west-1.amazonaws.com/rochaytv/Noel-Edmonds-HD1080p.mov">
+                    <img src="${this.BASEURL}resources/noel-edmonds.lcr" width="548" height="308" />
+                    <title class="whiteText">Noel Edmonds</title>
+                  </lockup>
+                  <lockup vidUrl="https://s3-eu-west-1.amazonaws.com/rochaytv/Better-Capital-HD1080p.mov">
+                    <img src="${this.BASEURL}resources/mark-aldridge.lcr" width="548" height="308" />
+                    <title class="whiteText">Mark Aldridge</title>
+                  </lockup>
+                  <lockup vidUrl="https://s3-eu-west-1.amazonaws.com/rochaytv/Stewart-McIntyre-HD1080p.mov">
+                    <img src="${this.BASEURL}resources/stewart-mcintyre.lcr" width="548" height="308" />
+                    <title class="whiteText">Stewart McIntyre</title>
+                  </lockup>
+                  <lockup vidUrl="https://s3-eu-west-1.amazonaws.com/rochaytv/Richard-Lambert-HD1080p.mov">
+                    <img src="${this.BASEURL}resources/richard-lambert.lcr" width="548" height="308" />
+                    <title class="whiteText">Richard Lambert</title>
+                  </lockup>
+                  <lockup vidUrl="https://s3-eu-west-1.amazonaws.com/rochaytv/Nigel-Stuart-HD1080p.mov">
+                    <img src="${this.BASEURL}resources/nigel-stuart.lcr" width="548" height="308" />
+                    <title class="whiteText">Nigel Stewart</title>
+                  </lockup>
+                </section>
+              </grid>
+            </relatedContent>
+          </listItemLockup>
+          <listItemLockup>
+            <title>Luxury Insights...</title>
+            <decorationLabel>1</decorationLabel>
+            <relatedContent>
+              <grid>
+                <section>
+                  <lockup vidUrl="https://s3-eu-west-1.amazonaws.com/rochaytv/LLX-Promo-HD1080p.mov">
+                    <img src="${this.BASEURL}resources/llx-thumbnail.lcr" width="548" height="308" />
+                    <title class="whiteText">London Luxury Expo</title>
+                  </lockup>
+                </section>
+              </grid>
+            </relatedContent>
+          </listItemLockup>
+          <listItemLockup>
+            <title>Luxury Events...</title>
+            <decorationLabel>1</decorationLabel>
+            <relatedContent>
+              <grid>
+                <section>
+                  <lockup vidUrl="https://s3-eu-west-1.amazonaws.com/rochaytv/LLX-Promo-HD1080p.mov">
+                    <img src="${this.BASEURL}resources/llx-thumbnail.lcr" width="548" height="308" />
+                    <title class="whiteText">London Luxury Expo</title>
+                  </lockup>
+                </section>
+              </grid>
+            </relatedContent>
+          </listItemLockup>
+        </section>
+      </list>
+    </catalogTemplate>
+  </document>`
+}
