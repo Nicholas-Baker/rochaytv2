0ad4a1fb3d55260ff5bc576ea6c543e70150f893
NICHOLAS BAKER <nicholas@rochay.com>
2016-04-13 23:24:43 +0100

    typos

--- templates/rochaytv2-catalog.xml.js
+++ templates/rochaytv2-catalog.xml.js
@@ -82,11 +82,11 @@ var Template = function() { return `<?xml version="1.0" encoding="UTF-8" ?>
                   </lockup>
                   <lockup vidUrl="https://s3-eu-west-1.amazonaws.com/rochaytv/NicoleD1080p.mov">
                     <img src="${this.BASEURL}resources/Nicole.lcr" width="548" height="308" />
-                    <title class="whiteText">Nicole de Carle London</title>
+                    <title class="whiteText">Nichole de Carle London</title>
                   </lockup>
                   <lockup vidUrl="https://s3-eu-west-1.amazonaws.com/rochaytv/SpirioHD1080p.mov">
                     <img src="${this.BASEURL}resources/Spirio.lcr" width="548" height="308" />
-                    <title class="whiteText">Steinay - Spirio</title>
+                    <title class="whiteText">Steinway - Spirio</title>
                   </lockup>
                 </section>
               </grid>
